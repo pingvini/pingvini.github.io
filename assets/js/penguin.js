10a8593d4cc9ea6fa2c4be22777baf0c1323eef3
Tomaž Toplak <tomaz.toplak@gmail.com>
2013-08-16 18:01:54 +0200

    Made changes that circumvent most weird stuff that old browsers send.

--- assets/js/penguin.js
+++ assets/js/penguin.js
@@ -1,8 +1,6 @@
 var siteUrl = 'http://'+(document.location.hostname||document.location.host);
-
 /*It is better to save selectors that are used often*/
 var $links = $(".nav > li > a").parent(); 
-var $navbar = $(".navbar-toggle");
 var $contentdiv=$('#content-div');
 var $infinite=$(".infinite-image-cover, .infinite-image-inner-top");
 var $infiniteimage=$('.infinite-image');
@@ -10,32 +8,33 @@ var $infiniteimage=$('.infinite-image');
 $(document).ready(function() { 
   $(document).delegate('a[href^="/"],a[href^="'+siteUrl+'"]', "click", function(e) {
       e.preventDefault();
-      History.pushState({}, "", this.pathname);
+      History.pushState({}, "", this.pathname=='' ? '/': this.pathname);
   });
   History.Adapter.bind(window, 'statechange', function(){
-      var State = History.getState();
-      $.get(State.url, function(data){
+      var url = History.getState().hash;
+      url = url.replace('./', ''); //old browsers
+      $.get(url, function(data){
           /*Get infinite image if not there yet*/
           if($infiniteimage.is(':empty'))
           {
-            $infiniteimage.html($(data).filter('.infinite-image').html());   
+            $infiniteimage.html($(data).filter('.infinite-image').html());
             $infinite=$(".infinite-image-cover, .infinite-image-inner-top");         
             $infinite.hide(); 
           }          
           /*load new content*/
           document.title = $(data).filter('title').text();
           $contentdiv.html($(data).filter('#content-div'));
+          
           /*set active menu item*/          
           $links.removeClass("active");
-          var page = State.hash.
-          						substring(0, Math.min(4, State.hash.length)).replace(/\//g, '');
+          var page = url.substring(1, url.length).split('\/')[0];
           $(".nav > li > a[href*="+page+"]").parent().addClass("active");
           /*close navbar if open*/
-          if(!$navbar.hasClass('collapsed') && $navbar.is(":visible")){
-            $navbar.click(); 
+          if($(".nav-collapse").is(":visible") && $(".navbar-toggle").is(":visible")){
+            $(".navbar-toggle").click();
           }
           /*show/hide image if neaded*/
-          if(State.hash!='/'){
+          if(url.replace('/', '')!=''){
             $infinite.slideUp("fast"); 
           }
           else{
@@ -44,4 +43,5 @@ $(document).ready(function() {
       });
   });
 });
+
 
