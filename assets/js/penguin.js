753190b108894ce560991e682c4486971a482f37
Tomaž Toplak <tomaz.toplak@gmail.com>
2013-08-16 18:07:03 +0200

    Added additional support for old browsers. Since they display links iwth /#subpage users also bookmark them as such. So now when somebody uses an old browser and cals www.domain.com/#subpage he is shown the correct page. fixes #8

--- assets/js/penguin.js
+++ assets/js/penguin.js
@@ -4,27 +4,28 @@ var $links = $(".nav > li > a").parent();
 var $contentdiv=$('#content-div');
 var $infinite=$(".infinite-image-cover, .infinite-image-inner-top");
 var $infiniteimage=$('.infinite-image');
-
 $(document).ready(function() { 
+  if(document.location.hash.length>1){//old browsers.. load subpage if caled with /#name and not /name
+    getPageAjax(document.location.hash.replace(/[#]/g, '/'));
+  }
   $(document).delegate('a[href^="/"],a[href^="'+siteUrl+'"]', "click", function(e) {
       e.preventDefault();
       History.pushState({}, "", this.pathname=='' ? '/': this.pathname);
   });
   History.Adapter.bind(window, 'statechange', function(){
-      var url = History.getState().hash;
+      getPageAjax(History.getState().hash);
+  });  
+  function getPageAjax(url){  
       url = url.replace('./', ''); //old browsers
-      $.get(url, function(data){
-          /*Get infinite image if not there yet*/
-          if($infiniteimage.is(':empty'))
-          {
+      $.get(url, function(data){          
+          if($infiniteimage.is(':empty')){ /*Get infinite image if not there yet*/          
             $infiniteimage.html($(data).filter('.infinite-image').html());
             $infinite=$(".infinite-image-cover, .infinite-image-inner-top");         
             $infinite.hide(); 
           }          
           /*load new content*/
           document.title = $(data).filter('title').text();
-          $contentdiv.html($(data).filter('#content-div'));
-          
+          $contentdiv.html($(data).filter('#content-div'));          
           /*set active menu item*/          
           $links.removeClass("active");
           var page = url.substring(1, url.length).split('\/')[0];
@@ -36,12 +37,9 @@ $(document).ready(function() {
           /*show/hide image if neaded*/
           if(url.replace('/', '')!=''){
             $infinite.slideUp("fast"); 
-          }
-          else{
+          }else{
             $infinite.slideDown("fast"); 
           }
-      });
-  });
+      });    
+  }
 });
-
-
